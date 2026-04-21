4ef658e0e6a4cea4817c79c9298b8dbc210c5fac
sparshi jain <jainsparshi80@gmail.com>
2026-04-22 00:46:40 +0530

    Update sentiment_ml.py

--- ml/sentiment_ml.py
+++ ml/sentiment_ml.py
@@ -6,16 +6,16 @@ class MLSentiment:
         base = "."  # repo root
         self.tfidf = pickle.load(open(os.path.join(base, "tfidf_vectorizer.pkl"), "rb"))
         self.lr = pickle.load(open(os.path.join(base, "logistic_regression_model.pkl"), "rb"))
-        self.rf = pickle.load(open(os.path.join(base, "random_forest_model.pkl"), "rb"))
+
 
 
     def predict(self, text):
         X = self.tfidf.transform([text])
         lr_pred = self.lr.predict(X)[0]
-        rf_pred = self.rf.predict(X)[0]
+        
 
         if lr_pred == rf_pred:
-            return lr_pred, "High", lr_pred, rf_pred
+            return lr_pred, "High", lr_pred
         else:
-            return lr_pred, "Medium", lr_pred, rf_pred
+            return lr_pred, "Medium", lr_pred
 
